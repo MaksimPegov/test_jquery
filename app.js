e0c3b9c70132e2119bb0fc072649f3467144469d
Антон Пегов <apegov@kld.neolant>
2016-10-22 17:27:07 +0200

    Добавление друзей реализовано

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 $(document).ready(app)
 function app() {
-    var txt = 'akjshjklf';
-    var newTxt = '<li>kill me!!</li>'
 
+    var girls = ["lisa","virf"]
+    var boys = ["max","andrey"]    
 
-    $("#add").on("click",function(){
-        $("#box").append(newTxt);
-    })
+    showFriends();
 
-    $("#kill").on("click",function(){
-        $("li").text(txt);
+    $("i.fa").on("click",addFriend);
+    function addFriend(){
+        var text = $("input").val();
+        if(text == ""){
+            $("ul").text("new")
+            return false;
+        }
+        var element = $(this);
+        if(element.hasClass('fa-female')){
+            girls.push(text);
+            showFriends();
+            // Сохраняю девочек в Хранилище
+            localStorage.setItem('girls',girls);
+        }else{
+            boys.push(text);
+            showFriends();
+            // Сохраняю мальчиков в Хранилище 
+            localStorage.setItem('boys',boys);
+        }
         
-    })
+    }
+    
+    function showFriends() {
+        var papa = $("div.male").find("ul") 
+        var mama = $("div.female").find("ul")
+        mama.empty()
+        papa.empty()
+        for(var i = 0; i < girls.length; i+=1){
+            var tmp = $("<li>"+girls[i]+"</li>")
+            mama.append(tmp)
+        }
+        for(var i = 0; i < boys.length; i+=1){
+            var tmp = $("<li>"+boys[i]+"</li>")
+            papa.append(tmp)
+        }
+    }
+    
 }
